d0a50935a233179039ffcf272a5d107de167046e
zeork7 <thomas.costa@efrei.net>
2024-04-11 10:29:43 +0200

    lab2

---
+++ src/weather_api.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import os
+
+# Importez votre fonction get_weather() depuis votre fichier main
+from main import get_weather
+
+app = Flask(__name__)
+
+@app.route('/weather', methods=['GET'])
+def weather():
+    latitude = request.args.get('LATTITUDE')
+    longitude = request.args.get('LONGITUDE')
+
+    print(latitude, longitude)
+    
+    if not latitude or not longitude:
+        return jsonify({'error': 'Latitude or longitude missing'}), 400
+    
+    try:
+        latitude = float(latitude)
+        longitude = float(longitude)
+    except ValueError:
+        return jsonify({'error': 'Latitude or longitude is not a valid number'}), 400
+    
+    try:
+        weather_description, temperature = get_weather(latitude, longitude)
+        return jsonify({'weather_description': weather_description, 'temperature': temperature}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
